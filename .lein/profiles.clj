a45d221b769b00be1081298535fb141a3416fc3a
robertzhouxh <robertzhouxh@gmail.com>
2018-08-30 18:46:48 +0800

    update lein confg:

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[lein-try "0.4.3"]
-                  [cider/cider-nrepl "0.14.0"]
-                  [refactor-nrepl "2.2.0"]]}}
+                  [cider/cider-nrepl "0.18.0"]
+                  [refactor-nrepl "2.4.0"]]}}
